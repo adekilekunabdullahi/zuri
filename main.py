1dd3762751704919a30494ce812dfdb19fe34c21
adekilekunabdullahi <106445363+adekilekunabdullahi@users.noreply.github.com>
2022-06-08 11:18:15 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,20 +1,39 @@
-# Check if two words are anagrams 
-# Example:
-# find_anagrams("hello", "check") --> False
-# find_anagrams("below", "elbow") --> True
 
+import random
 
-def find_anagram(word, anagram):
-    # [assignment] Add your code here
-   if(sorted(word)==sorted(anagram)):
-          return True
+print("WELCOME TO ROCK-PAPER-SCISSORS GAME ")
+R='rock'
+P="paper"
+S="scissors"
+varieties=['R','P','S']
+
+while True:
+   userChoice=input('Enter a leter from  R,S,P: ')
+   if userChoice not in varieties:
+      print("error,try again ")
+      continue
    else:
-          return False
-print(find_anagram('listen','silent'))
-print(find_anagram('good','god'))
+      break
+cpuChoice=random.choice(varieties)  
+print('Cpu turn: '+cpuChoice)
+if userChoice== "R" and cpuChoice=='R':
+   print("draw")
+elif userChoice=="P" and cpuChoice=="P":
+   print("draw")
+elif userChoice=="S" and cpuChoice=="S":
+   print('draw')
+elif userChoice=="R" and cpuChoice=="S":
+   print("YOU WIN")
+elif userChoice=="S" and cpuChoice=="P":
+   print("YOU WIN")
+elif userChoice=="P" and cpuChoice=="R":
+   print("YOU WIN")
+elif userChoice=="S" and cpuChoice=="R":
+   print("YOU LOSE")
+elif userChoice=="P" and cpuChoice=="S":
+   print("YOU LOSE")
+else:
+   print("YOU LOSE")
 
 
-
-
-        
 
